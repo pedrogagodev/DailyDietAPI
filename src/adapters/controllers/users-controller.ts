8eeb75a83804e66898102966edbc0d829899970e
Pedro Oliveira <pedrogagodev@gmail.com>
2025-03-01 23:01:12 -0400

    feat: creates users-controllers with create method

---
+++ src/adapters/controllers/users-controller.ts
@@ -0,0 +1,26 @@
+import type { User } from "@/core/entities/user";
+import type { CreateUserUseCase } from "@/core/use-cases/users/create-user";
+import type { FastifyReply, FastifyRequest } from "fastify";
+
+interface CreateUserRequest {
+  name: string;
+  email: string;
+}
+
+export class UsersController {
+  private createUserUseCase: CreateUserUseCase;
+
+  constructor(createUserUseCase: CreateUserUseCase) {
+    this.createUserUseCase = createUserUseCase;
+  }
+  async create(request: FastifyRequest, reply: FastifyReply): Promise<void> {
+
+    try {
+      const { name, email } = request.body as CreateUserRequest;
+      const user: User = await this.createUserUseCase.create({ name, email });
+      return reply.status(201).send(user);
+    } catch (error) {
+      reply.status(400).send({ error: (error as Error).message });
+    }
+  }
+}
