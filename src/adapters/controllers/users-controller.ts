2e64d89f1f223f90fd20b375ad78abbf5d87e7ea
Pedro Oliveira <pedrogagodev@gmail.com>
2025-03-03 16:22:55 -0400

    feat: add password in user controller

--- src/adapters/controllers/users-controller.ts
+++ src/adapters/controllers/users-controller.ts
@@ -5,6 +5,7 @@ import type { FastifyReply, FastifyRequest } from "fastify";
 interface CreateUserRequest {
   name: string;
   email: string;
+  password: string;
 }
 
 export class UsersController {
@@ -14,10 +15,13 @@ export class UsersController {
     this.createUserUseCase = createUserUseCase;
   }
   async create(request: FastifyRequest, reply: FastifyReply): Promise<void> {
-
     try {
-      const { name, email } = request.body as CreateUserRequest;
-      const user: User = await this.createUserUseCase.create({ name, email });
+      const { name, email, password } = request.body as CreateUserRequest;
+      const user: User = await this.createUserUseCase.create({
+        name,
+        email,
+        password,
+      });
       return reply.status(201).send(user);
     } catch (error) {
       reply.status(400).send({ error: (error as Error).message });
