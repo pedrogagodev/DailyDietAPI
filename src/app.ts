e6514f3b414626292b61ff8343bf110ac2ce3f4b
Pedro Oliveira <pedrogagodev@gmail.com>
2025-03-09 20:03:02 -0400

    feat: handling global errors

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,26 @@
 import fastify from "fastify";
 import { usersRoutes } from "@/routes/user-routes";
 import { mealsRoutes } from "@/routes/meal-routes";
+import { ZodError } from "zod";
+import { env } from "./config/env";
 
 export const app = fastify();
 
 app.register(usersRoutes);
 app.register(mealsRoutes);
+
+app.setErrorHandler((error, _request, reply) => {
+  if (error instanceof ZodError) {
+    return reply
+      .status(400)
+      .send({ message: "Validation error.", issues: error.format() });
+  }
+
+  if (env.NODE_ENV !== "production") {
+    console.error(error);
+  } else {
+    // TODO: Here we should log an external app (Sentry/Datadog)
+  }
+
+  return reply.status(500).send({ message: "Internal server error." });
+});
