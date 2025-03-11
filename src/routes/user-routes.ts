b5028f60810c48210b7004ccb043520b291a2db5
Pedro Oliveira <pedrogagodev@gmail.com>
2025-03-10 20:28:32 -0400

    feat: add login route to usersRoutes for user authentication

--- src/routes/user-routes.ts
+++ src/routes/user-routes.ts
@@ -1,6 +1,9 @@
+import { authenticate } from "@/http/controllers/users/authenticate";
 import { register } from "@/http/controllers/users/register";
 import type { FastifyInstance } from "fastify";
 
 export async function usersRoutes(app: FastifyInstance) {
   app.post("/register", register);
+
+  app.post("/login", authenticate);
 }
