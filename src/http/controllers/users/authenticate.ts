b4d496673a09e25eb2007fe8ef8f051d5600b86f
Pedro Oliveira <pedrogagodev@gmail.com>
2025-03-10 20:28:27 -0400

    feat: add authenticate controller with validation for email and password

---
+++ src/http/controllers/users/authenticate.ts
@@ -0,0 +1,37 @@
+import { InvalidCredentialsError } from "@/errors/invalid-credentials-error";
+import { makeAuthenticateUseCase } from "@/use-cases/factories/make-authenticate-use-case";
+import type { FastifyReply, FastifyRequest } from "fastify";
+import { z } from "zod";
+
+export async function authenticate(
+  request: FastifyRequest,
+  reply: FastifyReply
+) {
+  const authenticateBodySchema = z.object({
+    email: z.string().email({ message: "Invalid email" }),
+    password: z
+      .string()
+      .min(6, { message: "Invalid password" })
+      .regex(/[A-Z]/, "Password must contain at least one uppercase letter")
+      .regex(/[0-9]/, "Password must contain at least one number"),
+  });
+
+  const { email, password } = authenticateBodySchema.parse(request.body);
+
+  try {
+    const authenticateUseCase = makeAuthenticateUseCase();
+
+    await authenticateUseCase.execute({
+      email,
+      password,
+    });
+  } catch (err) {
+    if (err instanceof InvalidCredentialsError) {
+      reply.status(400).send({ message: err.message });
+    }
+
+    throw err;
+  }
+
+  return reply.status(200).send();
+}
