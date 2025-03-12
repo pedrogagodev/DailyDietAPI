ef1f70b946836d1f75825c28f410e8a876e8c60d
Pedro Oliveira <pedrogagodev@gmail.com>
2025-03-12 01:10:35 -0400

    test: add unit tests for GetTotalMealsNumberUseCase to verify total meals count functionality

---
+++ src/use-cases/meals/get-total-meals-number.spec.ts
@@ -0,0 +1,31 @@
+import { InMemoryMealsRepository } from "@/repositories/in-memory/in-memory-meals-repository";
+import { beforeEach, describe, expect, it } from "vitest";
+import { CreateMealUseCase } from "./create-meal";
+import { getTotalMealsNumberUseCase } from "./get-total-meals-number";
+
+let mealsRepository: InMemoryMealsRepository;
+let createMealCase: CreateMealUseCase;
+let getTotalMealsNumberCase: getTotalMealsNumberUseCase;
+
+describe("Get Total Meals Number Use Case", () => {
+  beforeEach(() => {
+    mealsRepository = new InMemoryMealsRepository();
+    createMealCase = new CreateMealUseCase(mealsRepository);
+    getTotalMealsNumberCase = new getTotalMealsNumberUseCase(mealsRepository);
+  });
+
+  it("should be able to get total meals number", async () => {
+    const createdMeal = await createMealCase.execute({
+      name: "Bread with eggs",
+      isOnDiet: true,
+      userId: "user-1",
+      description: "A simple breakfast",
+    });
+
+    const totalMealsNumber = await getTotalMealsNumberCase.execute({
+      userId: createdMeal.meal.userId,
+    });
+
+    expect(totalMealsNumber).toBe(1);
+  });
+});
