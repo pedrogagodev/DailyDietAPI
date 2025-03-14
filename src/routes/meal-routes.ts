8f6dbe87ab15ff3ec7b884b238b82aef7343e092
Pedro Oliveira <pedrogagodev@gmail.com>
2025-03-14 16:02:53 -0400

    feat: add route to get total number of meals for a user

--- src/routes/meal-routes.ts
+++ src/routes/meal-routes.ts
@@ -1,6 +1,7 @@
 import { createMeal } from "@/http/controllers/meals/create-meal";
 import { deleteMeal } from "@/http/controllers/meals/delete-meal";
 import { getMealInfo } from "@/http/controllers/meals/get-meal-info";
+import { getTotalMealsNumber } from "@/http/controllers/meals/get-total-meals-number";
 import { listMeals } from "@/http/controllers/meals/list-meals";
 import { updateMeal } from "@/http/controllers/meals/update-meal";
 import { verifyJWT } from "@/http/middlewares/verify-jwt";
@@ -13,7 +14,7 @@ export async function mealsRoutes(app: FastifyInstance) {
   app.post("/meals", createMeal);
 
   app.get("/me/:userId/meals", listMeals);
-
+  app.get("/me/:userId/meals/total", getTotalMealsNumber);
   app.get("/meals/:mealId", getMealInfo);
 
   app.put("/meals/:mealId", updateMeal);
