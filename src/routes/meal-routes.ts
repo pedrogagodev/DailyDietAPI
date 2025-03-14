35f7a2fb89791f13ea28245698adc4725edb760d
Pedro Oliveira <pedrogagodev@gmail.com>
2025-03-14 13:40:31 -0400

    feat: add JWT verification middleware to meal routes

--- src/routes/meal-routes.ts
+++ src/routes/meal-routes.ts
@@ -1,10 +1,13 @@
 import { createMeal } from "@/http/controllers/meals/create-meal";
 import { listMeals } from "@/http/controllers/meals/list-meals";
 import { updateMeal } from "@/http/controllers/meals/update-meal";
+import { verifyJWT } from "@/http/middlewares/verify-jwt";
 
 import type { FastifyInstance } from "fastify";
 
 export async function mealsRoutes(app: FastifyInstance) {
+  app.addHook("onRequest", verifyJWT);
+
   app.post("/meals", createMeal);
 
   app.get("/meals/:userId", listMeals);
