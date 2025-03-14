99ee25fdb2598e4dae562cd19ebf8667eb3a126e
Pedro Oliveira <pedrogagodev@gmail.com>
2025-03-14 01:29:19 -0400

    feat: add route to list meals for a specific user

--- src/routes/meal-routes.ts
+++ src/routes/meal-routes.ts
@@ -1,6 +1,10 @@
 import { createMeal } from "@/http/controllers/meals/create-meal";
+import { listMeals } from "@/http/controllers/meals/list-meals";
+
 import type { FastifyInstance } from "fastify";
 
 export async function mealsRoutes(app: FastifyInstance) {
   app.post("/meals", createMeal);
+
+  app.get("/meals/:userId", listMeals);
 }
