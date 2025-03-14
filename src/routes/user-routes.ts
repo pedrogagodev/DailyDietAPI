30ffff5bba6a4f93edb9fc0080a38d1a8b7774cf
Pedro Oliveira <pedrogagodev@gmail.com>
2025-03-14 17:17:37 -0400

    feat: add route for refreshing JWT token to enhance authentication flow

--- src/routes/user-routes.ts
+++ src/routes/user-routes.ts
@@ -1,5 +1,6 @@
 import { authenticate } from "@/http/controllers/users/authenticate";
 import { profile } from "@/http/controllers/users/profile";
+import { refresh } from "@/http/controllers/users/refresh";
 import { register } from "@/http/controllers/users/register";
 import { verifyJWT } from "@/http/middlewares/verify-jwt";
 import type { FastifyInstance } from "fastify";
@@ -8,6 +9,7 @@ export async function usersRoutes(app: FastifyInstance) {
   app.post("/register", register);
 
   app.post("/login", authenticate);
+  app.patch("/token/refresh", refresh);
 
   app.get("/me", { onRequest: [verifyJWT] }, profile);
 }
