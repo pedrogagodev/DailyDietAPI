2db8a838552cb8735b5f0b7812007a11df4a5cda
Pedro Oliveira <pedrogagodev@gmail.com>
2025-03-14 01:02:18 -0400

    feat: rename factory function to create update meal use case

--- src/use-cases/factories/make-update-meal-use-case.ts
+++ src/use-cases/factories/make-update-meal-use-case.ts
@@ -1,7 +1,7 @@
 import { MethodsMealsRepository } from "@/repositories/db-meals-repository";
 import { UpdateMealUseCase } from "../meals/update-meal";
 
-export function makeDeleteMealUseCase() {
+export function makeUpdateMealUseCase() {
     const mealsRepository = new MethodsMealsRepository();
     const updateMealUseCase = new UpdateMealUseCase(mealsRepository);
 
