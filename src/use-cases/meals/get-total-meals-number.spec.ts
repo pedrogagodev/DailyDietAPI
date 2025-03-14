9554901086ef347985d5a95f9bd5356d3f08926b
Pedro Oliveira <pedrogagodev@gmail.com>
2025-03-13 22:07:13 -0400

    refactor: rename use case classes to follow PascalCase naming convention

--- src/use-cases/meals/get-total-meals-number.spec.ts
+++ src/use-cases/meals/get-total-meals-number.spec.ts
@@ -1,17 +1,17 @@
 import { InMemoryMealsRepository } from "@/repositories/in-memory/in-memory-meals-repository";
 import { beforeEach, describe, expect, it } from "vitest";
 import { CreateMealUseCase } from "./create-meal";
-import { getTotalMealsNumberUseCase } from "./get-total-meals-number";
+import { GetTotalMealsNumberUseCase } from "./get-total-meals-number";
 
 let mealsRepository: InMemoryMealsRepository;
 let createMealCase: CreateMealUseCase;
-let getTotalMealsNumberCase: getTotalMealsNumberUseCase;
+let getTotalMealsNumberCase: GetTotalMealsNumberUseCase;
 
 describe("Get Total Meals Number Use Case", () => {
   beforeEach(() => {
     mealsRepository = new InMemoryMealsRepository();
     createMealCase = new CreateMealUseCase(mealsRepository);
-    getTotalMealsNumberCase = new getTotalMealsNumberUseCase(mealsRepository);
+    getTotalMealsNumberCase = new GetTotalMealsNumberUseCase(mealsRepository);
   });
 
   it("should be able to get total meals number", async () => {
