9464f36ac4c3b3ee2d0628ef28e3957888c701b6
Pedro Oliveira <pedrogagodev@gmail.com>
2025-03-14 10:55:13 -0400

    feat: add route to update meal by ID

--- src/routes/meal-routes.ts
+++ src/routes/meal-routes.ts
@@ -1,5 +1,6 @@
 import { createMeal } from "@/http/controllers/meals/create-meal";
 import { listMeals } from "@/http/controllers/meals/list-meals";
+import { updateMeal } from "@/http/controllers/meals/update-meal";
 
 import type { FastifyInstance } from "fastify";
 
@@ -7,4 +8,6 @@ export async function mealsRoutes(app: FastifyInstance) {
   app.post("/meals", createMeal);
 
   app.get("/meals/:userId", listMeals);
+
+  app.put("/meals/:id", updateMeal);
 }
