e131a36f6bfe3f4eea77cb62c8532062c68fa40d
Pedro Oliveira <pedrogagodev@gmail.com>
2025-03-14 14:52:26 -0400

    feat: validate request parameters for listing and updating meals using Zod schemas

--- src/http/controllers/meals/list-meals.ts
+++ src/http/controllers/meals/list-meals.ts
@@ -1,12 +1,13 @@
 import { makeListMealsUseCase } from "@/use-cases/factories/make-list-meals-use-case";
 import type { FastifyReply, FastifyRequest } from "fastify";
-
-interface ListMealsQuery {
-  userId: string;
-}
+import { z } from "zod";
 
 export async function listMeals(request: FastifyRequest, reply: FastifyReply) {
-  const { userId } = request.query as ListMealsQuery;
+  const listMealsSchema = z.object({
+    userId: z.string().min(1, { message: "Please, provide a user id" }),
+  });
+
+  const { userId } = listMealsSchema.parse(request.query);
   try {
     const listMealsUseCase = makeListMealsUseCase();
 
--- src/http/controllers/meals/update-meal.ts
+++ src/http/controllers/meals/update-meal.ts
@@ -1,15 +1,28 @@
 import { makeUpdateMealUseCase } from "@/use-cases/factories/make-update-meal-use-case";
 import type { FastifyReply, FastifyRequest } from "fastify";
+import { z } from "zod";
 
 interface UpdateMealBody {
   name?: string;
   description?: string | null;
-isOnDiet?: boolean;
+  isOnDiet?: boolean;
 }
 
 export async function updateMeal(request: FastifyRequest, reply: FastifyReply) {
-  const { name, description, isOnDiet } = request.body as UpdateMealBody;
-  const { id } = request.query as { id: string };
+  const updateMealBodySchema = z.object({
+    name: z.string().optional(),
+    description: z.string().optional(),
+    isOnDiet: z.boolean().optional(),
+  });
+
+  const updateMealQuerySchema = z.object({
+    id: z.string().min(1, { message: "Please, provide a meal id" }),
+  });
+
+  const { id } = updateMealQuerySchema.parse(request.query);
+  const { name, description, isOnDiet } = updateMealBodySchema.parse(
+    request.body
+  );
   try {
     const updateMealUseCase = makeUpdateMealUseCase();
 
