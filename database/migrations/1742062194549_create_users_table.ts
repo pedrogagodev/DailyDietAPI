1f47bb293b0ea4b03c07c80aee79715a67393df7
Pedro Oliveira <pedrogagodev@gmail.com>
2025-03-15 18:38:25 -0400

    feat: add uuid-ossp extension to users table migration

--- database/migrations/1742062194549_create_users_table.ts
+++ database/migrations/1742062194549_create_users_table.ts
@@ -3,6 +3,9 @@ import type { ColumnDefinitions, MigrationBuilder } from "node-pg-migrate";
 export const shorthands: ColumnDefinitions | undefined = undefined;
 
 export async function up(pgm: MigrationBuilder): Promise<void> {
+
+  pgm.createExtension('uuid-ossp', { ifNotExists: true });
+
   pgm.createTable("users", {
     id: {
       type: "uuid",
@@ -27,4 +30,5 @@ export async function up(pgm: MigrationBuilder): Promise<void> {
 
 export async function down(pgm: MigrationBuilder): Promise<void> {
   pgm.dropTable("users");
+  pgm.dropExtension("uuid-ossp");
 }
