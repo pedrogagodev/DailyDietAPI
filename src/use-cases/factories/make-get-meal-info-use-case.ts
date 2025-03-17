6464a69719cf8f6c193c3faa52d17a1f0739e9b2
Pedro Oliveira <pedrogagodev@gmail.com>
2025-03-17 19:13:55 -0300

    feat: include UsersRepository in GetMealInfoUseCase for enhanced meal information retrieval

--- src/use-cases/factories/make-get-meal-info-use-case.ts
+++ src/use-cases/factories/make-get-meal-info-use-case.ts
@@ -1,9 +1,14 @@
 import { MethodsMealsRepository } from "@/repositories/db-meals-repository";
+import { MethodsUsersRepository } from "@/repositories/db-users-repository";
 import { GetMealInfoUseCase } from "../meals/get-meal-info";
 
 export function makeGetMealInfoUseCase() {
   const mealsRepository = new MethodsMealsRepository();
-  const getMealInfoUseCase = new GetMealInfoUseCase(mealsRepository);
+  const usersRepository = new MethodsUsersRepository();
+  const getMealInfoUseCase = new GetMealInfoUseCase(
+    mealsRepository,
+    usersRepository
+  );
 
   return getMealInfoUseCase;
 }
