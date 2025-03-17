b6d4603696f6cf7b36828037dfa8d29ae727cf91
Pedro Oliveira <pedrogagodev@gmail.com>
2025-03-17 20:41:31 -0300

    refactor: simplify authentication logic by removing redundant try-catch block and improving code readability

--- src/http/controllers/users/authenticate.ts
+++ src/http/controllers/users/authenticate.ts
@@ -1,4 +1,3 @@
-import { InvalidCredentialsError } from "@/errors/invalid-credentials-error";
 import { makeAuthenticateUseCase } from "@/use-cases/factories/make-authenticate-use-case";
 import type { FastifyReply, FastifyRequest } from "fastify";
 import { z } from "zod";
@@ -17,52 +16,43 @@ export async function authenticate(
   });
 
   const { email, password } = authenticateBodySchema.parse(request.body);
+  const authenticateUseCase = makeAuthenticateUseCase();
 
-  try {
-    const authenticateUseCase = makeAuthenticateUseCase();
+  const { user } = await authenticateUseCase.execute({
+    email,
+    password,
+  });
 
-    const { user } = await authenticateUseCase.execute({
-      email,
-      password,
-    });
-
-    const token = await reply.jwtSign(
-      {
-        role: user.role,
+  const token = await reply.jwtSign(
+    {
+      role: user.role,
+    },
+    {
+      sign: {
+        sub: user.id,
       },
-      {
-        sign: {
-          sub: user.id,
-        },
-      }
-    );
-
-    const refreshToken = await reply.jwtSign(
-      {
-        role: user.role,
-      },
-      {
-        sign: {
-          sub: user.id,
-          expiresIn: "7d",
-        },
-      }
-    );
-
-    return reply
-      .setCookie("refreshToken", refreshToken, {
-        path: "/",
-        secure: true,
-        sameSite: true,
-        httpOnly: true,
-      })
-      .status(200)
-      .send({ token });
-  } catch (err) {
-    if (err instanceof InvalidCredentialsError) {
-      reply.status(400).send({ message: err.message });
     }
+  );
 
-    throw err;
-  }
+  const refreshToken = await reply.jwtSign(
+    {
+      role: user.role,
+    },
+    {
+      sign: {
+        sub: user.id,
+        expiresIn: "7d",
+      },
+    }
+  );
+
+  return reply
+    .setCookie("refreshToken", refreshToken, {
+      path: "/",
+      secure: true,
+      sameSite: true,
+      httpOnly: true,
+    })
+    .status(200)
+    .send({ token });
 }
