5dba5340d259d0fe28a9d40e0484fd883472fdc2
Pedro Oliveira <pedrogagodev@gmail.com>
2025-03-17 19:14:26 -0300

    feat: include UsersRepository in GetTotalMealsNumberUseCase for improved meal retrieval functionality

--- src/use-cases/factories/make-get-total-meals-number-use-case.ts
+++ src/use-cases/factories/make-get-total-meals-number-use-case.ts
@@ -1,10 +1,14 @@
 import { MethodsMealsRepository } from "@/repositories/db-meals-repository";
+import { MethodsUsersRepository } from "@/repositories/db-users-repository";
 import { GetTotalMealsNumberUseCase } from "../meals/get-total-meals-number";
 
 export function makeGetTotalMealsNumberUseCase() {
   const mealsRepository = new MethodsMealsRepository();
+  const usersRepository = new MethodsUsersRepository();
+
   const getTotalMealsNumberUseCase = new GetTotalMealsNumberUseCase(
-    mealsRepository
+    mealsRepository,
+    usersRepository
   );
 
   return getTotalMealsNumberUseCase;
