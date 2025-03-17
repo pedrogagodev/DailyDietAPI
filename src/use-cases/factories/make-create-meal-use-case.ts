b4ba64accbf4a0ed7c78adb05dbda4944d36e53d
Pedro Oliveira <pedrogagodev@gmail.com>
2025-03-17 19:13:31 -0300

    feat: update CreateMealUseCase to include UsersRepository for enhanced meal creation functionality

--- src/use-cases/factories/make-create-meal-use-case.ts
+++ src/use-cases/factories/make-create-meal-use-case.ts
@@ -1,9 +1,14 @@
 import { MethodsMealsRepository } from "@/repositories/db-meals-repository";
+import { MethodsUsersRepository } from "@/repositories/db-users-repository";
 import { CreateMealUseCase } from "../meals/create-meal";
 
 export function makeCreateMealUseCase() {
   const mealsRepository = new MethodsMealsRepository();
-  const createMealUseCase = new CreateMealUseCase(mealsRepository);
+  const usersRepository = new MethodsUsersRepository();
+  const createMealUseCase = new CreateMealUseCase(
+    mealsRepository,
+    usersRepository
+  );
 
   return createMealUseCase;
 }
