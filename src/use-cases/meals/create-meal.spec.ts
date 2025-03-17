75431f667902641d557bcf5335652679debddda4
Pedro Oliveira <pedrogagodev@gmail.com>
2025-03-17 20:41:06 -0300

    fix: enhance CreateMealUseCase tests to include user creation and validate user existence with UserNotFoundError handling

--- src/use-cases/meals/create-meal.spec.ts
+++ src/use-cases/meals/create-meal.spec.ts
@@ -1,24 +1,51 @@
 import { beforeEach, describe, expect, it } from "vitest";
 import { CreateMealUseCase } from "./create-meal";
 import { InMemoryMealsRepository } from "@/repositories/in-memory/in-memory-meals-repository";
+import { InMemoryUsersRepository } from "@/repositories/in-memory/in-memory-users-repository";
+import { UserNotFoundError } from "@/errors/user-not-found";
 
 let mealsRepository: InMemoryMealsRepository;
+let usersRepository: InMemoryUsersRepository;
 let createMealCase: CreateMealUseCase;
 
 describe("Create Meal Use Case", () => {
   beforeEach(() => {
     mealsRepository = new InMemoryMealsRepository();
-    createMealCase = new CreateMealUseCase(mealsRepository);
+    usersRepository = new InMemoryUsersRepository();
+    createMealCase = new CreateMealUseCase(mealsRepository, usersRepository);
   });
 
   it("should be able to create meal", async () => {
-    const { meal } = await createMealCase.execute({
-      name: "Bread with eggs",
+    const user = await usersRepository.create({
+      name: "John Doe",
+      email: "john.doe@example.com",
+      password_hash: "hashed-password",
+    });
+
+      const { meal } = await createMealCase.execute({
+        name: "Bread with eggs",
       isOnDiet: true,
-      userId: "user-1",
+      userId: user.id,
       description: "A simple breakfast",
     });
 
     expect(meal.id).toEqual(expect.any(String));
+  });
+
+  it("should not be able to create meal with wrong user id", async () => {
+      await usersRepository.create({
+      name: "Wrong User",
+      email: "wrong@example.com",
+      password_hash: "hashed-password",
+    });
+
+    await expect(
+      createMealCase.execute({
+        name: "Bread with eggs",
+        isOnDiet: true,
+        userId: "wrong-user-id",
+        description: "A simple breakfast",
+      })
+    ).rejects.instanceOf(UserNotFoundError);
   });
 });
