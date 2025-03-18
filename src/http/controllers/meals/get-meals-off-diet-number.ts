8e0918fb2c11257dcf5f85ae8ba46cf22035f6fa
Pedro Oliveira <pedrogagodev@gmail.com>
2025-03-18 20:03:53 -0300

    test: enhance GetMealsOffDietNumber tests to ensure only meals belonging to the authenticated user are counted and refactor user data handling

--- src/http/controllers/meals/get-meals-off-diet-number.ts
+++ src/http/controllers/meals/get-meals-off-diet-number.ts
@@ -1,21 +1,16 @@
-import { ResourceNotFoundError } from "@/errors/resource-not-found-error";
 import { makeGetMealsOffDietNumberUseCase } from "@/use-cases/factories/make-get-meals-off-diet-use-case";
 import type { FastifyReply, FastifyRequest } from "fastify";
-import { z } from "zod";
 
 export async function getMealsOffDietNumber(
   request: FastifyRequest,
   reply: FastifyReply
 ) {
-  const getMealsOffDietNumberSchema = z.object({
-    userId: z.string().uuid({ message: "Invalid user id." }),
-  });
-
-  const { userId } = getMealsOffDietNumberSchema.parse(request.params);
+  const userId = request.user.sub;
   const getMealsOffDietNumberUseCase = makeGetMealsOffDietNumberUseCase();
 
   const mealsNumber = await getMealsOffDietNumberUseCase.execute({
     userId,
+    requestingUserId: userId,
   });
 
   return reply.status(200).send({ mealsNumber });
