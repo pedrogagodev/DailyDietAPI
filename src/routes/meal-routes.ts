ad8cf1df33934dc28fc3f2808eacb7aa6a5a37a2
Pedro Oliveira <pedrogagodev@gmail.com>
2025-03-18 16:03:20 -0300

    refactor: apply meal ownership middleware to meal routes for enhanced security

--- src/routes/meal-routes.ts
+++ src/routes/meal-routes.ts
@@ -7,8 +7,8 @@ import { getMealsOnDietNumber } from "@/http/controllers/meals/get-meals-on-diet
 import { getTotalMealsNumber } from "@/http/controllers/meals/get-total-meals-number";
 import { listMeals } from "@/http/controllers/meals/list-meals";
 import { updateMeal } from "@/http/controllers/meals/update-meal";
+import { ensureMealOwnershipMiddleware } from "@/http/middlewares/ensure-meal-ownership";
 import { verifyJWT } from "@/http/middlewares/verify-jwt";
-
 import type { FastifyInstance } from "fastify";
 
 export async function mealsRoutes(app: FastifyInstance) {
@@ -21,9 +21,21 @@ export async function mealsRoutes(app: FastifyInstance) {
   app.get("/me/meals/on-diet", getMealsOnDietNumber);
   app.get("/me/meals/off-diet", getMealsOffDietNumber);
   app.get("/me/meals/longest-on-diet-sequence", getLongestOnDietSequence);
-  app.get("/meals/:mealId", getMealInfo);
+  app.get(
+    "/meals/:mealId",
+    { onRequest: [ensureMealOwnershipMiddleware] },
+    getMealInfo
+  );
 
-  app.put("/meals/:mealId", updateMeal);
+  app.put(
+    "/meals/:mealId",
+    { onRequest: [ensureMealOwnershipMiddleware] },
+    updateMeal
+  );
 
-  app.delete("/meals/:mealId", deleteMeal);
+  app.delete(
+    "/meals/:mealId",
+    { onRequest: [ensureMealOwnershipMiddleware] },
+    deleteMeal
+  );
 }
