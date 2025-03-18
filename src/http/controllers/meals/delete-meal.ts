e17d9a0620fc88b775c6941bb6ec9be6c151173d
Pedro Oliveira <pedrogagodev@gmail.com>
2025-03-18 16:40:47 -0300

    refactor: enhance delete meal use case and controller to include user authorization checks and validate meal ownership

--- src/http/controllers/meals/delete-meal.ts
+++ src/http/controllers/meals/delete-meal.ts
@@ -1,5 +1,3 @@
-import { MealNotFoundError } from "@/errors/meal-not-found";
-import { UnauthorizedAccessError } from "@/errors/unauthorized-access-error";
 import { makeDeleteMealUseCase } from "@/use-cases/factories/make-delete-meal-use-case";
 import type { FastifyReply, FastifyRequest } from "fastify";
 import { z } from "zod";
@@ -10,8 +8,9 @@ export async function deleteMeal(request: FastifyRequest, reply: FastifyReply) {
   });
 
   const { mealId } = deleteMealSchema.parse(request.params);
+  const userId = request.user.sub;
   const deleteMealUseCase = makeDeleteMealUseCase();
-  await deleteMealUseCase.execute({ id: mealId });
+  await deleteMealUseCase.execute({ id: mealId, requestingUserId: userId });
 
   return reply.status(200).send({ message: "Meal deleted successfully" });
 }
