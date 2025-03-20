8dc2a1bd1d688d5206cb156737e064c27d59c17f
Pedro Oliveira <pedrogagodev@gmail.com>
2025-03-20 10:05:12 -0300

    feat: integrate Swagger documentation and CORS support into the Fastify app

--- src/app.ts
+++ src/app.ts
@@ -5,6 +5,9 @@ import fastifyJwt from "@fastify/jwt";
 import fastify from "fastify";
 import { env } from "./config/env";
 import { errorHandler } from "./http/middlewares/error-handler";
+import fastifySwagger from "@fastify/swagger";
+import fastifySwaggerUi from "@fastify/swagger-ui";
+import fastifyCors from "@fastify/cors";
 
 export const app = fastify();
 
@@ -18,6 +21,38 @@ app.register(fastifyJwt, {
   sign: {
     expiresIn: "10m",
   },
+});
+
+app.register(fastifyCors, {origin: "*"})
+
+app.register(fastifySwagger, {
+  swagger: {
+      info: {
+          title: 'Daily Diet API',
+          description: 'API to manage daily diet',
+          version: '1.0.0'
+      },
+      host: 'localhost:3333',
+      schemes: ['http'],
+      consumes: ['application/json'],
+      produces: ['application/json'],
+      securityDefinitions: {
+          bearerAuth: {
+              type: 'apiKey',
+              name: 'Authorization',
+              in: 'header'
+          }
+      }
+  }
+});
+
+app.register(fastifySwaggerUi, {
+  routePrefix: '/docs',
+  uiConfig: {
+      docExpansion: 'list',
+      deepLinking: false
+  },
+  staticCSP: true
 });
 
 app.register(fastifyCookie);
