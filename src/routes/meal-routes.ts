f7c7e4b33d52f3b39593a9f8cd9944dd28e5125f
Pedro Oliveira <pedrogagodev@gmail.com>
2025-03-21 15:53:02 -0300

    feat: implement OpenAPI documentation and Zod validation for total meals number route

--- src/routes/meal-routes.ts
+++ src/routes/meal-routes.ts
@@ -13,6 +13,7 @@ import {
   createMealBodySchema,
   createMealResponseSchema,
 } from "@/schemas/meals/create-meal-schema";
+import { getTotalMealsNumberResponseSchema } from "@/schemas/meals/get-total-meals-schema";
 import { listMealsResponseSchema } from "@/schemas/meals/list-meals-schema";
 import type { FastifyInstance } from "fastify";
 import type { FastifyZodOpenApiTypeProvider } from "fastify-zod-openapi";
@@ -44,7 +45,17 @@ export async function mealsRoutes(app: FastifyInstance) {
     },
     handler: listMeals,
   });
-  app.get("/me/meals/total", getTotalMealsNumber);
+  app.withTypeProvider<FastifyZodOpenApiTypeProvider>().route({
+    method: "GET",
+    url: "/me/meals/total",
+    schema: {
+      tags: ["meals"],
+      summary: "Get total meals number",
+      description: "Get total meals number",
+      response: getTotalMealsNumberResponseSchema,
+    },
+    handler: getTotalMealsNumber,
+  });
   app.get("/me/meals/on-diet", getMealsOnDietNumber);
   app.get("/me/meals/off-diet", getMealsOffDietNumber);
   app.get("/me/meals/longest-on-diet-sequence", getLongestOnDietSequence);
