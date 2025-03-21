a5076988ba6da234b523144d368635df9f2b6925
Pedro Oliveira <pedrogagodev@gmail.com>
2025-03-20 22:53:02 -0300

    refactor: simplify user registration by removing validation schema and using TypeScript type for request body

--- src/http/controllers/users/register.ts
+++ src/http/controllers/users/register.ts
@@ -1,20 +1,15 @@
-import { UserAlreadyExistsError } from "@/errors/user-already-exists-error";
 import { makeRegisterUseCase } from "@/use-cases/factories/make-register-use-case";
 import type { FastifyReply, FastifyRequest } from "fastify";
-import { z } from "zod";
+
+type RegisterBody = {
+  name: string;
+  email: string;
+  password: string;
+};
 
 export async function register(request: FastifyRequest, reply: FastifyReply) {
-  const createAccountSchema = z.object({
-    name: z.string().min(1, { message: "Please, provide a name" }),
-    email: z.string().email({ message: "Invalid email" }),
-    password: z
-      .string()
-      .min(6, { message: "Invalid password" })
-      .regex(/[A-Z]/, "Password must contain at least one uppercase letter")
-      .regex(/[0-9]/, "Password must contain at least one number"),
-  });
-
-  const { name, email, password } = createAccountSchema.parse(request.body);
+  
+  const { name, email, password } = request.body as RegisterBody;
 
   const registerUserCase = makeRegisterUseCase();
 
