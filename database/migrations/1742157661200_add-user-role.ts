b12ab3e9c5f9d61049f81fdc6d6b4c915106603d
Pedro Oliveira <pedrogagodev@gmail.com>
2025-03-21 20:59:13 -0300

    refactor: improve user role migration to check for existing ENUM type before creation and ensure proper cleanup

--- database/migrations/1742157661200_add-user-role.ts
+++ database/migrations/1742157661200_add-user-role.ts
@@ -3,9 +3,15 @@ import type { ColumnDefinitions, MigrationBuilder } from "node-pg-migrate";
 export const shorthands: ColumnDefinitions | undefined = undefined;
 
 export async function up(pgm: MigrationBuilder): Promise<void> {
-  pgm.sql(`
-    CREATE TYPE IF NOT EXISTS "user_role" AS ENUM ('ADMIN', 'USER');
+  const result = await pgm.db.query(`
+    SELECT 1 FROM pg_type WHERE typname = 'user_role';
   `);
+
+  if (result.rowCount === 0) {
+    pgm.sql(`
+      CREATE TYPE "user_role" AS ENUM ('ADMIN', 'USER');
+    `);
+  }
 
   pgm.addColumn("users", {
     role: {
@@ -18,8 +24,13 @@ export async function up(pgm: MigrationBuilder): Promise<void> {
 
 export async function down(pgm: MigrationBuilder): Promise<void> {
   pgm.dropColumn("users", "role");
-
-  pgm.sql(`
-    DROP TYPE IF EXISTS "user_role";
+  const result = await pgm.db.query(`
+    SELECT 1 FROM pg_type WHERE typname = 'user_role';
   `);
+
+  if (result.rowCount === 1) {
+    pgm.sql(`
+      DROP TYPE "user_role";
+    `);
+  }
 }
