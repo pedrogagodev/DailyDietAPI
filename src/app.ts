475f90448903230ba41669fbc1ec13def513d0e1
Pedro Oliveira <pedrogagodev@gmail.com>
2025-03-20 22:52:23 -0300

    refactor: streamline Fastify app setup by consolidating Swagger and CORS registration, and updating validation and serialization configurations

--- src/app.ts
+++ src/app.ts
@@ -1,17 +1,54 @@
 import { mealsRoutes } from "@/routes/meal-routes";
 import { usersRoutes } from "@/routes/user-routes";
 import fastifyCookie from "@fastify/cookie";
+import fastifyCors from "@fastify/cors";
 import fastifyJwt from "@fastify/jwt";
-import fastify from "fastify";
-import { env } from "./config/env";
-import { errorHandler } from "./http/middlewares/error-handler";
 import fastifySwagger from "@fastify/swagger";
 import fastifySwaggerUi from "@fastify/swagger-ui";
-import fastifyCors from "@fastify/cors";
+import fastify from "fastify";
+import {
+  fastifyZodOpenApiPlugin,
+  fastifyZodOpenApiTransform,
+  fastifyZodOpenApiTransformObject,
+  serializerCompiler,
+  validatorCompiler,
+} from "fastify-zod-openapi";
+import type { ZodOpenApiVersion } from "zod-openapi";
+import { env } from "./config/env";
+import { errorHandler } from "./http/middlewares/error-handler";
 
 export const app = fastify();
 
+app.setValidatorCompiler(validatorCompiler);
+app.setSerializerCompiler(serializerCompiler);
+
 app.setErrorHandler(errorHandler);
+
+app.register(fastifyCors, { origin: "*" });
+
+app.register(fastifyZodOpenApiPlugin);
+app.register(fastifySwagger, {
+  openapi: {
+    info: {
+      title: "Daily Diet API",
+      description: "API to manage daily diet",
+      version: "1.0.0",
+    },
+    openapi: "3.0.3" satisfies ZodOpenApiVersion,
+  },
+  transform: fastifyZodOpenApiTransform,
+  transformObject: fastifyZodOpenApiTransformObject,
+});
+
+app.register(fastifySwaggerUi, {
+  routePrefix: "/docs",
+  uiConfig: {
+    docExpansion: "list",
+    deepLinking: false,
+  },
+  staticCSP: true,
+});
+
 app.register(fastifyJwt, {
   secret: env.JWT_SECRET,
   cookie: {
@@ -23,41 +60,7 @@ app.register(fastifyJwt, {
   },
 });
 
-app.register(fastifyCors, {origin: "*"})
-
-app.register(fastifySwagger, {
-  swagger: {
-      info: {
-          title: 'Daily Diet API',
-          description: 'API to manage daily diet',
-          version: '1.0.0'
-      },
-      host: 'localhost:3333',
-      schemes: ['http'],
-      consumes: ['application/json'],
-      produces: ['application/json'],
-      securityDefinitions: {
-          bearerAuth: {
-              type: 'apiKey',
-              name: 'Authorization',
-              in: 'header'
-          }
-      }
-  }
-});
-
-app.register(fastifySwaggerUi, {
-  routePrefix: '/docs',
-  uiConfig: {
-      docExpansion: 'list',
-      deepLinking: false
-  },
-  staticCSP: true
-});
-
 app.register(fastifyCookie);
 
 app.register(usersRoutes);
 app.register(mealsRoutes);
-
-
