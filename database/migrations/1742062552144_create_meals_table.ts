c61733c6ec1bd84a17da4c361736462685990442
Pedro Oliveira <pedrogagodev@gmail.com>
2025-03-21 20:57:27 -0300

    refactor: update meals table creation in migration to use SQL syntax for improved readability

--- database/migrations/1742062552144_create_meals_table.ts
+++ database/migrations/1742062552144_create_meals_table.ts
@@ -3,38 +3,20 @@ import type { ColumnDefinitions, MigrationBuilder } from "node-pg-migrate";
 const shorthands: ColumnDefinitions | undefined = undefined;
 
 export const up = async (pgm: MigrationBuilder): Promise<void> => {
-  pgm.createTable("meals", {
-    id: {
-      type: "uuid",
-      primaryKey: true,
-      default: pgm.func("public.uuid_generate_v4()"),
-    },
-    user_id: {
-      type: "uuid",
-      notNull: true,
-      references: '"users" (id)',
-      onDelete: "CASCADE",
-    },
-    name: { type: "varchar(255)", notNull: true },
-    description: { type: "text" },
-    date_time: {
-      type: "timestamp",
-      notNull: true,
-      default: pgm.func("CURRENT_TIMESTAMP"),
-    },
-    is_on_diet: { type: "boolean", notNull: true },
-    created_at: {
-      type: "timestamp",
-      notNull: true,
-      default: pgm.func("CURRENT_TIMESTAMP"),
-    },
-    updated_at: {
-      type: "timestamp",
-      notNull: true,
-      default: pgm.func("CURRENT_TIMESTAMP"),
-    },
-  });
-}
+  
+  pgm.sql(`
+    CREATE TABLE IF NOT EXISTS "meals" (
+      "id" uuid DEFAULT public.uuid_generate_v4() PRIMARY KEY,
+      "user_id" uuid NOT NULL REFERENCES "users" (id) ON DELETE CASCADE,
+      "name" varchar(255) NOT NULL,
+      "description" text,
+      "date_time" timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+      "is_on_diet" boolean NOT NULL,
+      "created_at" timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+      "updated_at" timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
+    );
+  `);
+};
 
 export const down = async (pgm: MigrationBuilder): Promise<void> => {
   pgm.dropTable("meals");
