a96ad42f8ce18bc873482f35eff3058267f3ba39
Pedro Oliveira <pedrogagodev@gmail.com>
2025-03-20 23:17:26 -0300

    refactor: remove validation schema in authentication controller and use TypeScript type for request body

--- src/http/controllers/users/authenticate.ts
+++ src/http/controllers/users/authenticate.ts
@@ -1,21 +1,16 @@
 import { makeAuthenticateUseCase } from "@/use-cases/factories/make-authenticate-use-case";
 import type { FastifyReply, FastifyRequest } from "fastify";
-import { z } from "zod";
+  
+type AuthenticateBody = {
+  email: string;
+  password: string;
+};
 
 export async function authenticate(
   request: FastifyRequest,
   reply: FastifyReply
 ) {
-  const authenticateBodySchema = z.object({
-    email: z.string().email({ message: "Invalid email" }),
-    password: z
-      .string()
-      .min(6, { message: "Invalid password" })
-      .regex(/[A-Z]/, "Password must contain at least one uppercase letter")
-      .regex(/[0-9]/, "Password must contain at least one number"),
-  });
-
-  const { email, password } = authenticateBodySchema.parse(request.body);
+  const { email, password } = request.body as AuthenticateBody;
   const authenticateUseCase = makeAuthenticateUseCase();
 
   const { user } = await authenticateUseCase.execute({
