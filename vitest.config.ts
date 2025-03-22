c2c08c6aa5cdcc6b995a50de0da982b0ba821a8f
Pedro Oliveira <pedrogagodev@gmail.com>
2025-03-22 18:34:32 -0300

    chore: update vitest configuration to set test sequence as non-concurrent

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,10 +1,13 @@
-import { defineConfig } from "vitest/config";
 import path from "node:path";
+import { defineConfig } from "vitest/config";
 
 export default defineConfig({
   test: {
     environment: "pg",
     dir: "src/http",
+    sequence: {
+      concurrent: false,
+    },
   },
   resolve: {
     alias: {
