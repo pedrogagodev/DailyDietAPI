3cd94c1b51e696437c6387b56ca12d794bc8d418
Pedro Oliveira <pedrogagodev@gmail.com>
2025-03-21 21:02:53 -0300

    refactor: enhance user role migration to check for existing role column and ENUM type before creation and ensure proper cleanup

--- database/migrations/1742157661200_add-user-role.ts
+++ database/migrations/1742157661200_add-user-role.ts
@@ -1,36 +1,52 @@
 import type { ColumnDefinitions, MigrationBuilder } from "node-pg-migrate";
-
 export const shorthands: ColumnDefinitions | undefined = undefined;
 
 export async function up(pgm: MigrationBuilder): Promise<void> {
-  const result = await pgm.db.query(`
+  const typeResult = await pgm.db.query(`
     SELECT 1 FROM pg_type WHERE typname = 'user_role';
   `);
 
-  if (result.rowCount === 0) {
+  if (!typeResult.rowCount) {
     pgm.sql(`
       CREATE TYPE "user_role" AS ENUM ('ADMIN', 'USER');
     `);
   }
 
-  pgm.addColumn("users", {
-    role: {
-      type: "user_role",
-      notNull: true,
-      default: "USER",
-    },
-  });
+  const columnResult = await pgm.db.query(`
+    SELECT 1 
+    FROM information_schema.columns 
+    WHERE table_name = 'users' AND column_name = 'role';
+  `);
+
+  if (!columnResult.rowCount) {
+    pgm.addColumn("users", {
+      role: {
+        type: "user_role",
+        notNull: true,
+        default: "USER",
+      },
+    });
+  }
 }
 
 export async function down(pgm: MigrationBuilder): Promise<void> {
-  pgm.dropColumn("users", "role");
-  const result = await pgm.db.query(`
+  const columnResult = await pgm.db.query(`
+    SELECT 1 
+    FROM information_schema.columns 
+    WHERE table_name = 'users' AND column_name = 'role';
+  `);
+
+  if (columnResult.rowCount && columnResult.rowCount > 0) {
+    pgm.dropColumn("users", "role");
+  }
+
+  const typeResult = await pgm.db.query(`
     SELECT 1 FROM pg_type WHERE typname = 'user_role';
   `);
 
-  if (result.rowCount === 1) {
+  if (typeResult.rowCount && typeResult.rowCount > 0) {
     pgm.sql(`
-      DROP TYPE "user_role";
+      DROP TYPE IF EXISTS "user_role";
     `);
   }
 }
