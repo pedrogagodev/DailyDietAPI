edec1ba188463b37a8875f787a66e615882f9cc3
Pedro Oliveira <pedrogagodev@gmail.com>
2025-04-05 09:15:52 -0300

    feat: add Login page in routes

--- frontend/src/Router.tsx
+++ frontend/src/Router.tsx
@@ -1,12 +1,14 @@
 import { Route, Routes } from "react-router";
 import Home from "./pages/Home";
 import Register from "./pages/Register";
+import Login from "./pages/Login";
 
 export function Router() {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/register" element={<Register />} />
+      <Route path="/login" element={<Login />} />
     </Routes>
   );
 }
