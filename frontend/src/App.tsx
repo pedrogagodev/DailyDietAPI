ef4a8e30825c8c2c6021456ad78bc0b9288b0d97
Pedro Oliveira <pedrogagodev@gmail.com>
2025-04-05 19:34:30 -0300

    feat: wrap application in QueryClientProvider and add Toaster for notifications

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,17 @@
+import { QueryClientProvider } from "@tanstack/react-query";
+import { Toaster } from "react-hot-toast";
 import { BrowserRouter } from "react-router";
 import { Router } from "./Router";
+
+import { queryClient } from "./services/query-client";
 
 export function App() {
   return (
     <BrowserRouter>
-      <Router />
+      <QueryClientProvider client={queryClient}>
+        <Toaster />
+        <Router />
+      </QueryClientProvider>
     </BrowserRouter>
   );
 }
