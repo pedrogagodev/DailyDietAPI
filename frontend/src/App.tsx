268b6a326f8afa7c9be51c019913d7c25a3acb19
Pedro Oliveira <pedrogagodev@gmail.com>
2025-04-06 17:21:30 -0300

    feat: use AuthProvider um App.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,14 +3,17 @@ import { Toaster } from "react-hot-toast";
 import { BrowserRouter } from "react-router";
 import { Router } from "./Router";
 
+import { AuthProvider } from "./contexts/AuthContext";
 import { queryClient } from "./services/query-client";
 
 export function App() {
   return (
     <BrowserRouter>
       <QueryClientProvider client={queryClient}>
-        <Toaster />
-        <Router />
+        <AuthProvider>
+          <Toaster />
+          <Router />
+        </AuthProvider>
       </QueryClientProvider>
     </BrowserRouter>
   );
