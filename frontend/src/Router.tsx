c3b27df2e299d8fa9c58affb8c8d281a2904c8ba
Pedro Oliveira <pedrogagodev@gmail.com>
2025-04-05 22:56:37 -0300

    feat: add layout in Router.tsx

--- frontend/src/Router.tsx
+++ frontend/src/Router.tsx
@@ -1,4 +1,5 @@
 import { Route, Routes } from "react-router";
+import { DefaultLayout } from "./layouts/DefaultLayout";
 import Dashboard from "./pages/Dashboard";
 import Home from "./pages/Home";
 import Login from "./pages/Login";
@@ -7,10 +8,12 @@ import Register from "./pages/Register";
 export function Router() {
   return (
     <Routes>
-      <Route path="/" element={<Home />} />
+      <Route element={<DefaultLayout />}>
+        <Route path="/" element={<Home />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+      </Route>
       <Route path="/register" element={<Register />} />
       <Route path="/login" element={<Login />} />
-      <Route path="/dashboard" element={<Dashboard />} />
     </Routes>
   );
 }
