688965c211009ee85297742b71eab1ec41b4728a
Pedro Oliveira <pedrogagodev@gmail.com>
2025-04-06 17:20:26 -0300

    feat: add interceptor in requests to use Bearer token in req header

--- frontend/src/services/httpClient.ts
+++ frontend/src/services/httpClient.ts
@@ -4,3 +4,13 @@ import axios from "axios";
 export const httpClient = axios.create({
   baseURL: env.VITE_API_URL,
 });
+
+httpClient.interceptors.request.use(config => {
+  const accessToken = localStorage.getItem("@DailyDiet:token");
+
+  if (accessToken) {
+    config.headers.Authorization = `Bearer ${accessToken}`;
+  }
+
+  return config;
+});
