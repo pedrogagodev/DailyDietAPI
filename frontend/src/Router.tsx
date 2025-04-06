2945b905716a3934174d6f1dacc3f1875da4a69a
Pedro Oliveira <pedrogagodev@gmail.com>
2025-04-06 17:19:28 -0300

    feat: use AuthGuard in Router.tsx

--- frontend/src/Router.tsx
+++ frontend/src/Router.tsx
@@ -1,4 +1,5 @@
 import { Route, Routes } from "react-router";
+import { AuthGuard } from "./layouts/AuthGuard";
 import { DefaultLayout } from "./layouts/DefaultLayout";
 import Dashboard from "./pages/Dashboard";
 import Home from "./pages/Home";
@@ -10,10 +11,14 @@ export function Router() {
     <Routes>
       <Route element={<DefaultLayout />}>
         <Route path="/" element={<Home />} />
-        <Route path="/dashboard" element={<Dashboard />} />
+        <Route element={<AuthGuard isPrivate={true} />}>
+          <Route path="/dashboard" element={<Dashboard />} />
+        </Route>
       </Route>
-      <Route path="/register" element={<Register />} />
-      <Route path="/login" element={<Login />} />
+      <Route element={<AuthGuard isPrivate={false} />}>
+        <Route path="/register" element={<Register />} />
+        <Route path="/login" element={<Login />} />
+      </Route>
     </Routes>
   );
 }
