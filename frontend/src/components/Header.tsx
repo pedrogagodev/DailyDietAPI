ba28f8c22a704cac5fb893385af6a21aba696e3b
Pedro Oliveira <pedrogagodev@gmail.com>
2025-04-06 17:22:18 -0300

    feat: add avatar and sign out button in header when user is logged

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -1,8 +1,12 @@
 import Logo from "@/assets/DailyDietLogo-Photoroom.svg";
+import { useAuth } from "@/hooks/useAuth";
 import { Link } from "react-router";
+import { Avatar, AvatarFallback, AvatarImage } from "./ui/avatar";
 import { Button } from "./ui/button";
 
 export function Header() {
+  const { isSignedIn, signout, data } = useAuth();
+
   return (
     <div className="flex items-center justify-between">
       <Link to="/">
@@ -11,9 +15,21 @@ export function Header() {
           <p className="font-bold text-xl">Daily Diet</p>
         </div>
       </Link>
-      <Link to="/login">
-        <Button className="mr-6 hover:cursor-pointer">Sign In</Button>
-      </Link>
+      {isSignedIn ? (
+        <div className="flex gap-4">
+          <Avatar className="h-9 w-9">
+            <AvatarImage />
+            <AvatarFallback>{data?.user?.name[0]}</AvatarFallback>
+          </Avatar>
+          <Button onClick={signout} className="mr-6 hover:cursor-pointer">
+            Sign Out
+          </Button>
+        </div>
+      ) : (
+        <Link to="/login">
+          <Button className="mr-6 hover:cursor-pointer">Sign In</Button>
+        </Link>
+      )}
     </div>
   );
 }
