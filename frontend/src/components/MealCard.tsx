c3fc6099295d7461d988c2ede8a76b4036f7103b
Pedro Oliveira <pedrogagodev@gmail.com>
2025-04-07 09:59:43 -0300

    feat: implement delete confirmation dialog in MealCard component

--- frontend/src/components/MealCard.tsx
+++ frontend/src/components/MealCard.tsx
@@ -1,4 +1,9 @@
+import { mealsService } from "@/services/mealsService";
+import { queryClient } from "@/services/query-client";
+import { useMutation } from "@tanstack/react-query";
 import { Circle } from "lucide-react";
+import { useState } from "react";
+import toast from "react-hot-toast";
 import { useNavigate } from "react-router";
 import { Button } from "./ui/button";
 import { Card, CardContent } from "./ui/card";
@@ -27,14 +32,30 @@ export function MealCard({
   isOnDiet,
   id,
 }: MealCardProps) {
+  const [isDialogOpen, setIsDialogOpen] = useState(false);
   const navigate = useNavigate();
 
   const handleClickEdit = () => {
     navigate(`/dashboard/meals/${id}`);
   };
+
+  const { mutate } = useMutation({
+    mutationFn: () => mealsService.deleteMeal({ id }),
+    onSuccess: () => {
+      queryClient.invalidateQueries({
+        queryKey: ["me", "listMeals"],
+      });
+      toast.success("Successfully deleted");
+      setIsDialogOpen(false);
+    },
+  });
+  const handleClickDelete = () => {
+    mutate();
+  };
+
   return (
-    <Dialog>
-      <DialogTrigger>
+    <Dialog open={isDialogOpen} onOpenChange={setIsDialogOpen}>
+      <DialogTrigger asChild>
         <Card className="hover:bg-gray-100 hover:cursor-pointer">
           <CardContent className="flex items-center justify-between">
             <div className="flex gap-4 items-center">
@@ -61,11 +82,30 @@ export function MealCard({
             <span>Is on diet? {isOnDiet ? "Yes" : "No"}</span>
           </DialogDescription>
         </DialogHeader>
-
-        <DialogClose className="flex justify-end gap-2 mt-6">
-          <Button onClick={handleClickEdit}>Edit</Button>
-          <Button variant="destructive">Delete</Button>
-        </DialogClose>
+        <div className="flex gap-4 mt-4">
+          <DialogClose>
+            <Button onClick={handleClickEdit}>Edit</Button>
+          </DialogClose>
+          <Dialog>
+            <DialogTrigger>
+              <Button variant="destructive">Delete</Button>
+            </DialogTrigger>
+            <DialogContent>
+              <DialogHeader>
+                <DialogTitle>Delete Meal</DialogTitle>
+                <DialogDescription>
+                  Are you sure you want to delete this meal?
+                </DialogDescription>
+              </DialogHeader>
+              <DialogClose className="flex justify-end gap-2 mt-6">
+                <Button variant="destructive" onClick={handleClickDelete}>
+                  Delete
+                </Button>
+                <Button>Cancel</Button>
+              </DialogClose>
+            </DialogContent>
+          </Dialog>
+        </div>
       </DialogContent>
     </Dialog>
   );
