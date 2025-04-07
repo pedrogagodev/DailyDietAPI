8d163539e2cb5856aa3aba4e34f42f9e81e1c83f
Pedro Oliveira <pedrogagodev@gmail.com>
2025-04-06 23:50:36 -0300

    feat: add edit meal in mealsService

--- frontend/src/services/mealsService/index.ts
+++ frontend/src/services/mealsService/index.ts
@@ -1,7 +1,9 @@
 import { createMeal } from "./createMeal";
+import { editMeal } from "./editMeal";
 import { listMeals } from "./listMeals";
 
 export const mealsService = {
   createMeal,
   listMeals,
+  editMeal
 };
