0e006b5e4f021800473d3e5057935fe7350ebdec
Pedro Oliveira <pedrogagodev@gmail.com>
2025-04-08 16:38:54 -0300

    feat: integrate Footer component into DefaultLayout for improved page structure

--- frontend/src/layouts/DefaultLayout.tsx
+++ frontend/src/layouts/DefaultLayout.tsx
@@ -1,3 +1,4 @@
+import { Footer } from "@/components/Footer";
 import { Header } from "@/components/Header";
 import { Outlet } from "react-router";
 
@@ -6,6 +7,7 @@ export function DefaultLayout() {
     <div>
       <Header />
       <Outlet />
+      <Footer />
     </div>
   );
 }
