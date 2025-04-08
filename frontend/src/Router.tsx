dc9e722a5e1a54f0ddbf7073aa88a47a96e223e6
Pedro Oliveira <pedrogagodev@gmail.com>
2025-04-08 16:40:47 -0300

    feat: update Router to replace Home page with LandingPage component for improved user experience

--- frontend/src/Router.tsx
+++ frontend/src/Router.tsx
@@ -4,7 +4,7 @@ import { DefaultLayout } from "./layouts/DefaultLayout";
 import { CreateMeal } from "./pages/CreateMeal";
 import Dashboard from "./pages/Dashboard";
 import { EditMeal } from "./pages/EditMeal";
-import Home from "./pages/Home";
+import LandingPage from "./pages/LandingPage";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
 
@@ -12,7 +12,7 @@ export function Router() {
   return (
     <Routes>
       <Route element={<DefaultLayout />}>
-        <Route path="/" element={<Home />} />
+      <Route path="/" element={<LandingPage />} />
         <Route element={<AuthGuard isPrivate={true} />}>
           <Route path="/dashboard" element={<Dashboard />}>
             <Route path="meals/create" element={<CreateMeal />} />
