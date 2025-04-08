a76453c8ade2f5761cc954ccf5507e4e28d978ed
Pedro Oliveira <pedrogagodev@gmail.com>
2025-04-08 16:38:33 -0300

    feat: redesign Header component for improved layout and user experience

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -7,31 +7,51 @@ import { ModeToggle } from "./ui/mode-toggle";
 
 export function Header() {
   const { isSignedIn, signout, data } = useAuth();
-
   return (
-    <div className="flex items-center justify-between">
-      <Link to="/">
-        <div className="flex items-center hover:cursor-pointer">
-          <img src={Logo} alt="" className="h-20 w-15" />
-          <p className="font-bold text-xl">Daily Diet</p>
-        </div>
-      </Link>
-      <ModeToggle />
-      {isSignedIn ? (
-        <div className="flex gap-4">
-          <Avatar className="h-9 w-9">
-            <AvatarImage />
-            <AvatarFallback>{data?.user?.name[0]}</AvatarFallback>
-          </Avatar>
-          <Button onClick={signout} className="mr-6 hover:cursor-pointer font-bold">
-            Sign Out
-          </Button>
-        </div>
-      ) : (
-        <Link to="/login">
-          <Button className="mr-6 hover:cursor-pointer">Sign In</Button>
+    <header className="sticky top-0 z-40 w-full border-b bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60">
+      <div className="container flex h-16 min-w-full items-center justify-between px-2 md:px-10">
+        <Link to="/">
+          <div className="flex items-center gap-2 hover:cursor-pointer">
+            <div className="size-10 rounded-full bg-primary/20 flex items-center justify-center">
+              <img
+                src={Logo}
+                alt="DailyDiet Logo"
+                className="h-14 w-14 md:h-20 md:w-15"
+              />
+            </div>
+            <span className="md:text-xl font-bold">Daily Diet</span>
+          </div>
         </Link>
-      )}
-    </div>
+        <div className="flex items-center gap-4">
+          <ModeToggle />
+          {isSignedIn ? (
+            <div className="flex gap-4">
+              <Avatar className="h-9 w-9">
+                <AvatarImage />
+                <AvatarFallback>{data?.user?.name[0]}</AvatarFallback>
+              </Avatar>
+              <Button
+                onClick={signout}
+                className="mr-6 hover:cursor-pointer font-bold"
+              >
+                Sign Out
+              </Button>
+            </div>
+          ) : (
+            <div className="flex gap-4 items-center justify-center">
+              <Link
+                to="/login"
+                className="text-sm font-medium hover:text-primary"
+              >
+                Log in
+              </Link>
+              <Button asChild>
+                <Link to="/register">Get Started</Link>
+              </Button>
+            </div>
+          )}
+        </div>
+      </div>
+    </header>
   );
 }
